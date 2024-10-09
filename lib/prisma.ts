197ccce1fd85eb935c4b5cbc292241e982305d5b
daichi.nagano <d.nagano1002@gmail.com>
2024-10-09 16:47:13 +0900

    fix build error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-if (!globalForPrisma.prisma) {
-  globalForPrisma.prisma = new PrismaClient();
-}
-prisma = globalForPrisma.prisma;
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+globalForPrisma.prisma = prisma;
 
 export default prisma;
